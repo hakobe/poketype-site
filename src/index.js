7b6c147bd6ea2b0c464cf07f1ef60263f1633c31
hakobe <hakobe@gmail.com>
2018-01-07 23:14:54 +0900

    選択できるように

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import './style.scss'
 // action
 const SELECT_POKE_TYPE = 'SELECT_POKE_TYPE'
 const RESET_POKE_TYPE = 'RESET_POKE_TYPE'
+const SELECT_MODE = 'SELECT_MODE'
 
 const selectPokeType = typ => {
   return {
@@ -21,6 +22,13 @@ const selectPokeType = typ => {
 const resetPokeType = () => {
   return {
     type: RESET_POKE_TYPE
+  }
+}
+
+const selectMode = mode => {
+  return {
+    type: SELECT_MODE,
+    selectedMode: mode
   }
 }
 
@@ -64,6 +72,15 @@ const SelectablePokeTypeButton = connect(
   mapDispatchToPropsPTB
 )(PokeTypeButton)
 
+const PokeTypeButtons = () => {
+  const buttons = poketype.TypesList.map(typ => (
+    <SelectablePokeTypeButton key={typ} typ={typ} />
+  ))
+  return <div className="poke-type-buttons">{buttons}</div>
+}
+
+const SelectablePokeTypeButtons = connect()(PokeTypeButtons)
+
 const mapDispatchToPropsRB = dispatch => {
   return {
     onClick: () => {
@@ -78,14 +95,43 @@ const ResetButton = connect(null, mapDispatchToPropsRB)(({ onClick }) => (
   </button>
 ))
 
-const PokeTypeButtons = () => {
-  const buttons = poketype.TypesList.map(typ => (
-    <SelectablePokeTypeButton key={typ} typ={typ} />
-  ))
-  return <div className="poke-type-buttons">{buttons}</div>
+const mapStateToPropsSM = state => {
+  return {
+    mode: state.mode
+  }
 }
 
-const SelectablePokeTypeButtons = connect()(PokeTypeButtons)
+const mapDispatchToPropsSM = dispatch => {
+  return {
+    onClick: mode => {
+      dispatch(selectMode(mode))
+    }
+  }
+}
+
+const SelectModeButton = connect(mapStateToPropsSM, mapDispatchToPropsSM)(
+  ({ mode, onClick }) => {
+    const deffenceCn = classNames({
+      defence: true,
+      active: mode === 'defence'
+    })
+    const offenceCn = classNames({
+      offence: true,
+      active: mode === 'offence'
+    })
+    return (
+      <div className="poke-mode-select">
+        <div className="msg">選択したタイプで...</div>
+        <button className={deffenceCn} onClick={() => onClick('defence')}>
+          ぼうぎょ
+        </button>
+        <button className={offenceCn} onClick={() => onClick('offence')}>
+          こうげき
+        </button>
+      </div>
+    )
+  }
+)
 
 const PokemonEffectivenessList = ({ effectivenesses }) => {
   const elems = effectivenesses.map(ef => {
@@ -104,45 +150,7 @@ const PokemonEffectivenessList = ({ effectivenesses }) => {
   return <ul className="poke-type-results">{elems}</ul>
 }
 
-const mapStateToPropsPEL = state => {
-  return {
-    effectivenesses: state.effectivenesses.filter(ef => ef.value !== 1)
-  }
-}
-
-const FilteredPokemonEffectivenessList = connect(mapStateToPropsPEL)(
-  PokemonEffectivenessList
-)
-
-const mapStateToPropsS = state => {
-  return {
-    selection: state.selection
-  }
-}
-
-const Selection = connect(mapStateToPropsS)(({ selection }) => {
-  const sels = [...selection].map(typ => {
-    return <li key={typ}>{typ}</li>
-  })
-  return <ul>{sels}</ul>
-})
-
-// reducer
-const getNextSelection = (current, typ) => {
-  const nextSelection = new Set(current)
-  if (nextSelection.has(typ)) {
-    nextSelection.delete(typ)
-  } else if (nextSelection.size < 2) {
-    nextSelection.add(typ)
-  }
-  return nextSelection
-}
-
-const calcEffectivenesses = selection => {
-  if (selection.size === 0) {
-    return []
-  }
-
+const calcEffectivenessesAsDefence = selection => {
   const target = poketype.createPokemon(...selection.values())
   return poketype.TypesList.map(typ => {
     const ef = poketype.calcEffectiveness(typ, target)
@@ -154,22 +162,88 @@ const calcEffectivenesses = selection => {
   })
 }
 
-const app = (state = { selection: new Set(), effectivenesses: [] }, action) => {
+const calcEffectivenessesAsOffence = selection => {
+  if (selection.size !== 1) {
+    return []
+  }
+
+  return poketype.TypesList.map(typ => {
+    const target = poketype.createPokemon(typ)
+    const ef = poketype.calcEffectiveness([...selection][0], target)
+    return {
+      message: ef.message,
+      value: ef.value,
+      skillType: typ
+    }
+  })
+}
+
+const calcEffectivenesses = (selection, mode) => {
+  if (selection.size === 0) {
+    return []
+  }
+
+  return (mode === 'defence'
+    ? calcEffectivenessesAsDefence(selection)
+    : calcEffectivenessesAsOffence(selection)
+  ).filter(e => e.value !== 1)
+}
+
+const mapStateToPropsPEL = state => {
+  return {
+    effectivenesses: calcEffectivenesses(state.selection, state.mode)
+  }
+}
+
+const FilteredPokemonEffectivenessList = connect(mapStateToPropsPEL)(
+  PokemonEffectivenessList
+)
+
+// reducer
+
+const getNextSelection = (current, typ, mode) => {
+  const nextSelection = new Set(current)
+  if (mode === 'defence') {
+    if (nextSelection.has(typ)) {
+      nextSelection.delete(typ)
+    } else if (nextSelection.size < 2) {
+      nextSelection.add(typ)
+    }
+  } else {
+    // offence
+    if (nextSelection.has(typ)) {
+      nextSelection.delete(typ)
+    } else {
+      nextSelection.clear()
+      nextSelection.add(typ)
+    }
+  }
+  return nextSelection
+}
+
+const app = (state = { selection: new Set(), mode: 'defence' }, action) => {
   switch (action.type) {
     case SELECT_POKE_TYPE:
       const nextSelection = getNextSelection(
         state.selection,
-        action.selectedType
+        action.selectedType,
+        state.mode
       )
 
       return {
-        selection: nextSelection,
-        effectivenesses: calcEffectivenesses(nextSelection)
+        ...state,
+        selection: nextSelection
       }
     case RESET_POKE_TYPE:
       return {
+        ...state,
+        selection: new Set()
+      }
+    case SELECT_MODE:
+      return {
+        ...state,
         selection: new Set(),
-        effectivenesses: []
+        mode: action.selectedMode
       }
     default:
       return state
@@ -183,6 +257,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <div className="poke-type-wrapper">
+        <SelectModeButton />
         <SelectablePokeTypeButtons />
         <ResetButton />
         <FilteredPokemonEffectivenessList />
