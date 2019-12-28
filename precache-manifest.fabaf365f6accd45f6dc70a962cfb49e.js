918a9abe14f00ff5407eacc19f17e6a009cfef93
hakobe <hakobe@gmail.com>
2019-12-28 23:37:41 +0900

    add pages

---
+++ precache-manifest.fabaf365f6accd45f6dc70a962cfb49e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed91e703ae2a912a49bbf0334c2c1ec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f40b68868fe9484a5628",
+    "url": "/static/css/main.822cf8a4.chunk.css"
+  },
+  {
+    "revision": "58100add2b612e1981e8",
+    "url": "/static/js/2.7679448b.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.7679448b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f40b68868fe9484a5628",
+    "url": "/static/js/main.7e34ada9.chunk.js"
+  },
+  {
+    "revision": "6c8dea4f0fc3a7bb4b46",
+    "url": "/static/js/runtime-main.c39bc683.js"
+  }
+]);
